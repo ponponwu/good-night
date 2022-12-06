04630892257e3b3be287558c34387d485347d4ae
Vincent <sheep0406@hotmail.com>
2022-12-06 18:53:08 +0800

    feat(alarm/factories): Add more trait for test case

--- spec/factories/alarms.rb
+++ spec/factories/alarms.rb
@@ -8,5 +8,10 @@ FactoryBot.define do
       slept_at { 2.weeks.from_now }
       awoke_at { 1.weeks.from_now }
     end
+
+    trait :yesterday_alarm do
+      slept_at { 1.days.ago }
+      awoke_at { 1.days.ago + 8.hour }
+    end
   end
 end
