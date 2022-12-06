d8493ebb2bf9466b7d7741997472a61ae4dd4fc1
Vincent <sheep0406@hotmail.com>
2022-12-06 15:53:06 +0800

    feat(alarm): Alarm add method for calculate duration

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -1,3 +1,10 @@
 class Alarm < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+  after_save :calculate_period_of_sleep, if: -> { awoke_at.present? && period_of_sleep.blank? }
+
+
+  def calculate_period_of_sleep
+    period = TimeDifference.between(slept_at, awoke_at).in_seconds.to_i
+    self.update!(period_of_sleep: period)
+  end
 end
