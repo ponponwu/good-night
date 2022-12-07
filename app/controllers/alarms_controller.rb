d14424d14297e1b8d8ce9bd1998b611f79e446b0
Vincent <sheep0406@hotmail.com>
2022-12-07 12:55:16 +0800

    feat(alarm_controller): Add pagination on alarm controller

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -2,11 +2,7 @@
 class AlarmsController < ApplicationController
   # GET /users/:user_id/alarms
   def index
-      alarms = Alarm.where(user_id: params[:user_id]).order(created_at: :desc)
-      #TODO: implement with jbuilder
-      render json: { status: 'ok' , data: alarms }
+    alarms = Alarm.where(user_id: params[:user_id]).order(created_at: :desc).page(params[:page])
+    render json: { status: 'ok', data: alarms }
   end
-
-
-
 end
