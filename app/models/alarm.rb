1a23ecd6e57e628ee50d3c40e5fcb3562224334a
Vincent <sheep0406@hotmail.com>
2022-12-07 12:56:28 +0800

    feat(alram, follow): Add pagination and relation validation setting

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -1,7 +1,7 @@
 class Alarm < ApplicationRecord
   belongs_to :user, optional: true
   after_save :calculate_period_of_sleep, if: -> { awoke_at.present? && period_of_sleep.blank? }
-
+  paginates_per 50
 
   def calculate_period_of_sleep
     period = TimeDifference.between(slept_at, awoke_at).in_seconds.to_i
