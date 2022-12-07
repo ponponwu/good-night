1a773614bcedeedd572bcab2f23cf3351ed833d9
Vincent <sheep0406@hotmail.com>
2022-12-07 11:28:33 +0800

    rspec(alarm): Change using time instead DateTime

--- spec/models/alarm_spec.rb
+++ spec/models/alarm_spec.rb
@@ -3,10 +3,10 @@ require "rails_helper"
 class AlarmTest < ActiveSupport::TestCase
   describe '#calculate_period_of_sleep' do
 
-    let(:alarm) { create(:alarm, slept_at: DateTime.new(2022, 12, 6), awoke_at: nil)}
+    let(:alarm) { create(:alarm, slept_at: Time.new(2022, 12, 6), awoke_at: nil)}
     context 'with after_save' do
       it 'awoke_at been updated' do
-        alarm.update(awoke_at: DateTime.new(2022, 12, 6, 12, 30))
+        alarm.update(awoke_at: Time.new(2022, 12, 6, 12, 30))
         expect(alarm.period_of_sleep).not_to be_nil
         expect(alarm.period_of_sleep).to eq 45000
       end
